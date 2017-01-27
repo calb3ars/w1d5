9db5120653c50636047fec2d255b1f4e5ed79a32
App Academy Student <student@appacademy.io>
2017-01-27 11:53:39 -0800

    Creates #bfs. Remember to use current_node within until loop. Don't use self

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -50,4 +50,14 @@ class PolyTreeNode
     nil
   end
 
+  def bfs(target_value)
+    queue = [self]
+    until queue.empty?
+      current_node = queue.shift
+      return current_node if current_node.value == target_value
+      queue += current_node.children
+    end
+    nil
+  end
+
 end
