1888f63a086d0e8ddb7491f74e4fbc2a87865c9c
App Academy Student <student@appacademy.io>
2017-01-27 14:23:53 -0800

    Implements #new_move_positions

--- knightpathfinder.rb
+++ knightpathfinder.rb
@@ -15,20 +15,23 @@ class KnightPathFinder
     possible_moves = []
     DELTAS.each do |delta|
       move = [pos[0] + delta[0], pos[1] + delta[1]]
-      possible_moves << move if in_board?(move)
+      possible_moves << move if (move[0].between?(0,7) && move[1].between?(0,7))
     end
     possible_moves
   end
 
-  def in_board?(pos)
-    return false unless pos[0] <= 7 && pos[1] <= 7
-  end
+  attr_accessor :start, :visited_positions
 
   def initialize(start_position)
     @start = start_position
     @end_pos = nil
     @move_tree = build_move_tree
-    @visited_positions = start_position
+    @visited_positions = [start]
+  end
+
+  def new_move_positions(pos)
+    new_positions = KnightPathFinder.valid_moves(pos) - visited_positions
+    @visited_positions += new_positions
   end
 
   def build_move_tree
